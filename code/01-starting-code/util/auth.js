734dabcbcd70ed320517f93065007a02c7d2d8cc
bevhatzl <beverleyhatzl@gmail.com>
2023-06-13 21:48:05 +1000

    make secret file work

--- code/01-starting-code/util/auth.js
+++ code/01-starting-code/util/auth.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
+import { API_KEY } from '../secrets';
 
-const API_KEY = 'AIzaSyCApAAH9OTN6cwxgxE7VR8Cpe-OL3w7rVo';
-
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
     {
