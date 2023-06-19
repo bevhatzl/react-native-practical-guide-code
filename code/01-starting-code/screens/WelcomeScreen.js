a6c31529cea6c6d7655ba3502fa66f60b9a8ecf8
bevhatzl <beverleyhatzl@gmail.com>
2023-06-19 20:41:43 +1000

    access protected resources

--- code/01-starting-code/screens/WelcomeScreen.js
+++ code/01-starting-code/screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMsg, setFetchedMsg] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://expense-tracker-9f094-default-rtdb.firebaseio.com/message.json?auth=' +
+          token
+      )
+      .then((response) => {
+        setFetchedMsg(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMsg}</Text>
     </View>
   );
 }
